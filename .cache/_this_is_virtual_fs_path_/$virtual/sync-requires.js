ad584be62234c971fece0eb67cee05fe785f9d68
dhnnnjy <m.dhnnjy@gmail.com>
2024-01-16 19:41:27 +0530

    16

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Dhananjay\\scaredllama\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\Dhananjay\\scaredllama\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Dhananjay\\scaredllama\\src\\pages\\index.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\Users\\Dhananjay\\scaredllama\\src\\pages\\using-typescript.tsx"))
+}
+
